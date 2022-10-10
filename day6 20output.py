a9c4278d4bf0cf08050652b2a62d195909b0089d
singamsai2002 <112846532+singamsai2002@users.noreply.github.com>
2022-10-10 10:34:07 +0530

    Add files via upload

---
+++ day6 20output.py
@@ -0,0 +1,34 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:/Users/vishnu/AppData/Local/Programs/Python/Python310/day6 20.py =
+enter the 1st number:12
+enter the 2nd number:19
+all the composite numbers between  12 and 19
+12 14 15 16 18 
+
+== RESTART: C:/Users/vishnu/AppData/Local/Programs/Python/Python310/day6 20.py =
+enter the 1st number:11
+enter the 2nd number:11
+all the composite numbers between  11 and 11
+
+== RESTART: C:/Users/vishnu/AppData/Local/Programs/Python/Python310/day6 20.py =
+enter the 1st number:20
+enter the 2nd number:10
+all the composite numbers between  20 and 10
+
+== RESTART: C:/Users/vishnu/AppData/Local/Programs/Python/Python310/day6 20.py =
+enter the 1st number:0
+enter the 2nd number:0
+all the composite numbers between  0 and 0
+
+== RESTART: C:/Users/vishnu/AppData/Local/Programs/Python/Python310/day6 20.py =
+enter the 1st number:-5
+enter the 2nd number:5
+all the composite numbers between  -5 and 5
+4 
+
+== RESTART: C:/Users/vishnu/AppData/Local/Programs/Python/Python310/day6 20.py =
+enter the 1st number:7
+enter the 2nd number:-12
+all the composite numbers between  7 and -12
