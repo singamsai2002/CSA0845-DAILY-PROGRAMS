cd49469101a6d2e9332de5d681db5b5606904349
singamsai2002 <112846532+singamsai2002@users.noreply.github.com>
2022-10-10 10:22:06 +0530

    Add files via upload

---
+++ day6 24.py
@@ -0,0 +1,8 @@
+x=int(input("enter the value of x:"))
+y=int(input("Enter the value of y:"))
+temp=x
+x=y
+y=temp
+print('the value of x after swapping:{}'.format(x))
+print('the value of y after swapping:{}'.format(y))            
+            
